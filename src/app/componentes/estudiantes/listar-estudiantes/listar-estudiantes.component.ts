b86badcb535b56a0099eb0b7ba172eb54d978e40
damd2212 <53495267+damd2212@users.noreply.github.com>
2023-02-04 15:28:29 -0500

    listo consultar, eliminar y listar estudainte

--- src/app/componentes/estudiantes/listar-estudiantes/listar-estudiantes.component.ts
+++ src/app/componentes/estudiantes/listar-estudiantes/listar-estudiantes.component.ts
@@ -1,6 +1,9 @@
+import { Direccion } from './../../../modelos/direccion';
 import { Component, OnInit } from '@angular/core';
 import { Estudiante } from 'src/app/modelos/estudiante';
 import { EstudiantesService } from 'src/app/servicios/estudiantes.service';
+import Swal from 'sweetalert2';
+import { Telefono } from 'src/app/modelos/telefono';
 
 @Component({
   selector: 'app-listar-estudiantes',
@@ -9,6 +12,10 @@ import { EstudiantesService } from 'src/app/servicios/estudiantes.service';
 })
 export class ListarEstudiantesComponent implements OnInit {
   estudiantes:Estudiante[] = [];
+
+  objEstudiante:Estudiante = new Estudiante();
+  objDireccion:Direccion = new Direccion();
+  objTelefonos: Telefono[] = [];
 
   constructor(private estudianteService:EstudiantesService) { }
 
@@ -20,8 +27,22 @@ export class ListarEstudiantesComponent implements OnInit {
   }
 
   eliminarEstudiante(id:number){
-    console.log("Presiono eliminar");
+    this.estudianteService.deleteEstudiante(id).subscribe(res=>{
+      if (res) {
 
+        Swal.fire('Esudiante eliminado','Estudiante con identificacion '+ id + ' eliminado correctamente!','success');
+      } else {
+        Swal.fire('Error de eliminacion','Error al eliminar el estudainte con codigo','success');
+      }
+      this.ngOnInit()
+    })
+
+  }
+
+  cargarEstudainte(estudiante:Estudiante){
+    this.objEstudiante = estudiante;
+    this.objDireccion = this.objEstudiante.objDireccion
+    this.objTelefonos = this.objEstudiante.listaTelefonos
   }
 
 }
