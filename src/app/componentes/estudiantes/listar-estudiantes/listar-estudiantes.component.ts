91e4e99833894bcf56bfdd08cda81ca00a32e63a
damd2212 <53495267+damd2212@users.noreply.github.com>
2023-02-04 19:29:48 -0500

    validaciones agregadas al eliminar un estudiante

--- src/app/componentes/estudiantes/listar-estudiantes/listar-estudiantes.component.ts
+++ src/app/componentes/estudiantes/listar-estudiantes/listar-estudiantes.component.ts
@@ -16,26 +16,45 @@ export class ListarEstudiantesComponent implements OnInit {
   objEstudiante:Estudiante = new Estudiante();
   objDireccion:Direccion = new Direccion();
   objTelefonos: Telefono[] = [];
+  public errores: string[] = [];
 
   constructor(private estudianteService:EstudiantesService) { }
 
   ngOnInit(): void {
     this.estudianteService.getEstudiantes().subscribe(res=>{
       console.log(res);
-      this.estudiantes = res
+      if (res!= null) {
+        this.estudiantes = res
+      }else{
+        this.estudiantes = []
+      }
     })
   }
 
   eliminarEstudiante(id:number){
     this.estudianteService.deleteEstudiante(id).subscribe(res=>{
       if (res) {
-
         Swal.fire('Esudiante eliminado','Estudiante con identificacion '+ id + ' eliminado correctamente!','success');
       } else {
         Swal.fire('Error de eliminacion','Error al eliminar el estudainte con codigo','success');
       }
       this.ngOnInit()
-    })
+    },err=>{
+      console.log(err.error);
+      console.log(err.error.codigoError);
+      console.log(typeof(err.error));
+      console.error('Codigo del error desde el back: ' + err.status);
+      this.errores = [];
+      let auxMensaje='';
+      Object.entries(err.error).forEach(([key,value])=>{
+        auxMensaje = key + ': ' + value
+        console.log(auxMensaje);
+        this.errores.push(auxMensaje)
+      })
+    }
+
+
+    )
 
   }
 
