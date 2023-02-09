b85e03cbaf0be8d1dbc4aca232b8e6a7d49b9ebe
FernandoCampoM <juancamm@unicauca.edu.co>
2023-02-09 03:08:33 -0500

    OK

--- src/app/servicios/estudiantes.service.ts
+++ src/app/servicios/estudiantes.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Estudiante } from '../modelos/estudiante';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -12,8 +14,47 @@ export class EstudiantesService {
   private urlEndPoint:string = "http://localhost:9090/api/estudiantes";
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private router: Router) { }
+  getStudent(id:number): Observable<Estudiante> {
+    return this.http.get<Estudiante>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(
+        e => {
+          this.router.navigate(['/estudiantes']);
+          console.error(e.error.mensaje);
+          swal.fire('Error al editar', e.error.mensaje, 'error');
+           return throwError(() => new Error(e));
+      })
+    );
+  }
+  create(cliente:Estudiante):Observable<Estudiante>{
+    return this.http.post<Estudiante>(this.urlEndPoint,cliente,{headers:this.httpHeaders}).pipe(
+      catchError(
+        e=>{
+          if (e.status == 400) {
+            return throwError(e);
+            }
+            console.log(e.error.mensaje);
+            swal.fire('Error al crear el estudiante', e.error.mensaje, 'error');
+            return throwError(e);
+        }
+      )
+    );
+  }
+  update(student: Estudiante): Observable<any> {
+    return this.http.put<any>(`${this.urlEndPoint}/${student.idPersona}`, student, { headers: this.httpHeaders }).pipe(
+      catchError(
+         e => {
 
+          if (e.status == 400) {
+            return throwError(e);
+          }
+
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(() => new Error(e));
+      })
+    );
+  }
   getEstudiantes():Observable<Estudiante[]>{
     return this.http.get<Estudiante[]>(this.urlEndPoint+'/todos');
   }
