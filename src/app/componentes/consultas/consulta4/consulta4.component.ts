9131c0ad21f3e34e1232990a2c0415092b93b9ba
Jorge <jorgeivan@unicauca.edu.co>
2023-02-09 14:40:49 -0500

    Consulta 4 realizada

--- src/app/componentes/consultas/consulta4/consulta4.component.ts
+++ src/app/componentes/consultas/consulta4/consulta4.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from 'src/app/modelos/estudiante';
+import { EstudiantesService } from 'src/app/servicios/estudiantes.service';
 
 @Component({
   selector: 'app-consulta4',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Consulta4Component implements OnInit {
 
-  constructor() { }
+  emailEstudiante : string = ""
+  estudiante : Estudiante = new Estudiante()
+  errores: string[] = []
+
+  constructor(private estudianteService : EstudiantesService) { }
 
   ngOnInit(): void {
   }
 
+  realizarConsulta(){
+    this.errores = []
+    this.estudiante = new Estudiante()
+    this.estudianteService.buscarEstudianteByEmail(this.emailEstudiante).subscribe(
+      res=>{
+        this.estudiante = res
+      },(err: HttpErrorResponse) => {
+        const map = new Map(Object.entries(err.error));
+        const vector = Array.from(map.values());
+        this.errores = vector as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+      }
+    )
+  }
 }
